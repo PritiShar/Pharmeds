a4929a20c6360e68fb13ec09b341391990deb058
Priti Sharma <git045.priti.sharma@gnkhalsa.edu.in>
2023-03-01 11:34:55 +0530

    changes i made

---
+++ app/src/main/java/com/priti/Pharmeds/UsersCategory.java
@@ -0,0 +1,14 @@
+package com.priti.Pharmeds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UsersCategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users_category);
+    }
+}
